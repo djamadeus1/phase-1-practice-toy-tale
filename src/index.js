fa704bcdbe9bd61022420cd96f28066eb6c4c77e
Ronald Roberts <djamadeus1@gmail.com>
2024-10-16 14:59:04 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,77 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  const toyForm = document.querySelector(".add-toy-form"); 
+
+toyForm.addEventListener("submit", (event) => {
+  event.preventDefault(); 
+  createToy(); 
+});
+
+  fetchToys();
+  function fetchToys() {
+    fetch('http://localhost:3000/toys')
+      .then(response => response.json())
+      .then(toys => {
+        toys.forEach(toy => renderToy(toy));
+      })
+  }
+
+  function renderToy(toy) {
+    const toyCollection = document.getElementById("toy-collection"); 
+  
+    const toyCard = document.createElement("div"); 
+    toyCard.className = "card"; 
+  
+    const toyName = document.createElement("h2"); 
+    toyName.textContent = toy.name; 
+  
+    const toyImage = document.createElement("img"); 
+    toyImage.src = toy.image; 
+    toyImage.className = "toy-avatar"; 
+  
+    const toyLikes = document.createElement("p"); 
+    toyLikes.textContent = `${toy.likes} Likes`; 
+  
+    const likeButton = document.createElement("button"); 
+    likeButton.className = "like-btn"; 
+    likeButton.id = toy.id; 
+    likeButton.textContent = "Like ❤️"; 
+  
+    toyCard.append(toyName, toyImage, toyLikes, likeButton);
+    
+    toyCollection.appendChild(toyCard);
+  }
+
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+
+ 
+function createToy() {
+  const nameInput = toyForm.querySelector('input[name="name"]');
+  const imageInput = toyForm.querySelector('input[name="image"]');
+  
+  const newToy = {
+    name: nameInput.value,
+    image: imageInput.value,
+    likes: 0
+  };
+  
+    fetch('http://localhost:3000/toys', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify(newToy)
+    })
+    .then(Response => Response.json())
+    .then(toy => {
+      renderToy(toy);
+      toyForm.reset();
+    });
+  }
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
